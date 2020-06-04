ffcb89d4d531c997fa7f6960b75b427c651f389e
Kravchenko Mikhail <kravchenko.mi@citilink.ru>
2020-06-04 20:41:14 +0300

    Добавил файлы 10 домашки

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Активация чекпоинтов
+/// </summary>
+public class CheckPoint : MonoBehaviour {
+    public Material commonMaterial;
+    public Material activeMaterial;
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            if (GameManager.Instance.lastCheckPoint) {
+                GameManager.Instance.lastCheckPoint.GetComponent<Renderer>().material = commonMaterial;
+            }
+            GameManager.Instance.lastCheckPoint = gameObject;
+            
+            GetComponent<Renderer>().material = activeMaterial;
+        }
+    }
+}
---
+++ Assets/Scripts/CollectItems.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Сбор предметов и отображение очков за сбор
+/// </summary>
+public class CollectItems : MonoBehaviour {
+    public Text countedItemsText;
+
+    private void Start() {
+        SetCountText();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            GameManager.Instance.collectedItems++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText() {
+        countedItemsText.text = $"Собрано предметов: {GameManager.Instance.collectedItems.ToString()}";
+    }
+}
